fb8a2e8562401c1c324fe84c0351127da020afb2
xFreed0m <13978578+xFreed0m@users.noreply.github.com>
2020-04-09 08:36:15 +0300

    v01

--- OSINTer.sh
+++ OSINTer.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #-Metadata----------------------------------------------------#
-#  Filename: OSINTer.sh             (Update: 2020-04-07)      #
+#  Filename: OSINTer.sh             (last Updated: 2020-04-07)#
 #-Info--------------------------------------------------------#
 #  Post-install script for installing OSINT tools             #
 #-Author(s)---------------------------------------------------#
 #  xFreed0m ~ https://betheadversary.com/                     #
 #  Most of the script is based (and parts copied and modified)#
-#  From G0tmi1k os-scripts                                    #
+#  From G0tmi1k os-scripts:                                   #
 # (https://github.com/g0tmi1k/os-scripts)                     #
 #-Operating System--------------------------------------------#
 #  Designed for: Debian based systems                         #
 #     Tested on: Ubuntu 19.10 Desktop [x64] (VM)              #
 
+#--- Only used for stats at the end
+start_time=$(date +%s)
 
 ##### (Cosmetic) Colour output
 RED="\033[01;31m"      # Issues/Errors
@@ -21,8 +23,32 @@ BLUE="\033[01;34m"     # Heading
 BOLD="\033[01;01m"     # Highlight
 RESET="\033[00m"       # Normal
 
+##### (Cosmetic) spinner to see that the command is running
+# TBD Placeholder
+
+
 STAGE=0                                                         # Where are we up to
 TOTAL=$( grep '(${STAGE}/${TOTAL})' $0 | wc -l );(( TOTAL-- ))  # How many things have we got todo
+
+# Let's log error to a log file for easier debug in case something failed
+LOGFILE=/tmp/OSINTer_$(date +%F_%T).log
+
+verbose=
+
+case "$1" in
+-v|--v|--ve|--ver|--verb|--verbo|--verbos|--verbose)
+    verbose=1
+    shift ;;
+esac
+
+if [ "$verbose" = 1 ]; then
+  # print all output also to console and logfile
+  exec >  >(tee -ia $LOGFILE)
+  exec 2> >(tee -ia $LOGFILE >&2)
+else
+  # print error to console and to the logfile
+  exec 2> >(tee -ia $LOGFILE >&2)
+fi
 
 ##### Check if we are running as root - else this script will fail (hard!)
 if [[ "${EUID}" -ne 0 ]]; then
@@ -82,10 +108,10 @@ else
 fi
 
 
-##### adding kali default network repositories ~ http://docs.kali.org/general-use/kali-linux-sources-list-repositories
+##### adding google & kali default network repositories ~ http://docs.kali.org/general-use/kali-linux-sources-list-repositories
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Adding kali rolling default OS ${GREEN}network repositories${RESET}"
 #--- validate sources key
-apt -y -qq install gnupg
+apt-get -y -qq install gnupg
 wget 'https://archive.kali.org/archive-key.asc'
 apt-key add archive-key.asc
 #--- To avoid kali sources taking over system packages
@@ -101,9 +127,13 @@ grep -q '^deb-src .* kali-rolling' "${file}" 2>/dev/null \
   || echo -e "deb-src http://http.kali.org/kali kali-rolling main contrib non-free" >> "${file}"
 #--- Disable CD repositories
 sed -i '/kali/ s/^\( \|\t\|\)deb cdrom/#deb cdrom/g' "${file}"
+#--- adding Google repository
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Adding Google ${GREEN}repositories${RESET}"
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo -e "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> "${file}"
 #--- incase we were interrupted
 dpkg --configure -a
-apt -qq update
+apt-get -qq update
 if [[ "$?" -ne 0 ]]; then
   echo -e ' '${RED}'[!]'${RESET}" There was an ${RED}issue accessing network repositories${RESET}" 1>&2
   echo -e " ${YELLOW}[i]${RESET} Are the remote network repositories ${YELLOW}currently being sync'd${RESET}?"
@@ -123,17 +153,17 @@ chattr +i "${file}" 2>/dev/null
 ##### Update OS from network repositories
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) ${GREEN}Updating OS${RESET} from network repositories"
 echo -e " ${YELLOW}[i]${RESET}  ...this ${BOLD}may take a while${RESET} depending on your Internet connection"
-for FILE in clean autoremove; do apt -y -qq "${FILE}"; done         # Clean up      clean remove autoremove autoclean
+for FILE in clean autoremove; do apt-get -y -qq "${FILE}"; done         # Clean up      clean remove autoremove autoclean
 export DEBIAN_FRONTEND=noninteractive
-apt -qq update && APT_LISTCHANGES_FRONTEND=none apt -o Dpkg::Options::="--force-confnew" -y dist-upgrade --fix-missing 2>&1 \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -qq update && APT_LISTCHANGES_FRONTEND=none apt-get -o Dpkg::Options::="--force-confnew" -y dist-upgrade --fix-missing 2>&1 \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
-for FILE in clean autoremove; do apt -y -qq "${FILE}"; done         # Clean up - clean remove autoremove autoclean
+for FILE in clean autoremove; do apt-get -y -qq "${FILE}"; done         # Clean up - clean remove autoremove autoclean
 
 ##### Install bash completion - all users
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}bash completion${RESET} ~ tab complete CLI commands"
-apt -y -qq install bash-completion \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install bash-completion \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 file=/etc/bash.bashrc; [ -e "${file}" ] && cp -n $file{,.bkup}   #~/.bashrc
 sed -i '/# enable bash completion in/,+7{/enable bash completion/!s/^#//}' "${file}"
 #--- Apply new configs
@@ -141,8 +171,8 @@ source "${file}"
 
 ##### Install git - all users
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}git${RESET} ~ revision control"
-apt -y -qq install git \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install git \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 #--- Set as default editor
 git config --global core.editor "vim"
 #--- Set as default mergetool
@@ -152,73 +182,78 @@ git config --global mergetool.prompt false
 #--- Set as default push
 git config --global push.default simple
 
+##### Install curl
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}curl${RESET}"
+apt-get -y -qq install curl \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
+
 ##### Install go
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}go${RESET} ~ programming language"
-apt -y -qq install golang \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install golang \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install libreoffice
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}LibreOffice${RESET} ~ GUI office suite"
-apt -y -qq install libreoffice \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install libreoffice \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install aptitude (needed for kali packages install)
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Aptitude${RESET}"
-apt -y -qq install aptitude \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install aptitude \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 #### TL OSINT Pkgs
 
 ##### Install GIMP
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}GIMP${RESET}"
-apt -y -qq install gimp \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install gimp \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install Flameshot
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Flameshot${RESET}"
-apt -y -qq install flameshot \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install flameshot \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install Shotwell
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Shotwell${RESET}"
-apt -y -qq install shotwell \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install shotwell \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install Audacity
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Audacity${RESET}"
-apt -y -qq install audacity \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install audacity \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install SoundConverter
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}SoundConverter${RESET}"
-apt -y -qq install soundconverter \
-|| echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install soundconverter \
+|| echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install Darktable
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}SpiderFoot${RESET}"
-apt -y -qq install darktable \
-|| echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install darktable \
+|| echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install Photoflare
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Photoflare${RESET}"
-apt -y -qq install photoflare \
-|| echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install photoflare \
+|| echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install SimpleScreenRecorder
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}SimpleScreenRecorder${RESET}"
-apt -y -qq install simplescreenrecorder \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install simplescreenrecorder \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install Peek
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Peek${RESET}"
-apt -y -qq install peek \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install peek \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 
 ##### Install TOR - TBD
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}TOR${RESET}"
-apt -y -qq install torbrowser-launcher \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install torbrowser-launcher \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install amass
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}amass${RESET}"
@@ -228,32 +263,31 @@ aptitude install -y -q -t kali-rolling amass \
 
 ##### Install Google Chrome (needed for hunchly)
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Google Chrome${RESET}"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
-apt install /tmp/./google-chrome-stable_current_amd64.deb \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install google-chrome-stable \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install maltego
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}maltego${RESET}"
 aptitude install -y -q -t kali-rolling maltego \
-|| echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+|| echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install stegosuite
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}stegosuite${RESET}"
-apt -y -qq install stegosuite \
-|| echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install stegosuite \
+|| echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 ##### Install python3 + pip3
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}python3 + pip3${RESET}"
-apt -y -qq install python3 python3-pip \
-|| echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install python3 python3-pip \
+|| echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 
 
 ##### source installs
 
 ##### Install theHarvester
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}theHarvester${RESET}"
-apt -y -qq install git \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install git \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 git clone -q -b master https://github.com/laramies/theHarvester.git /opt/theHarvester-git/ \
   || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
 pip3 install --progress-bar off -r /opt/theHarvester-git/requirements.txt \
@@ -264,8 +298,8 @@ popd >/dev/null
 
 ##### Install Photon
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Photon${RESET}"
-apt -y -qq install git \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install git \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 git clone -q -b master https://github.com/s0md3v/Photon.git /opt/photon-git/ \
   || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
 pip3 install --progress-bar off -r /opt/photon-git/requirements.txt \
@@ -276,8 +310,8 @@ popd >/dev/null
 
 ##### Install sherlock
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Sherlock${RESET}"
-apt -y -qq install git \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install git \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 git clone -q -b master https://github.com/sherlock-project/sherlock.git /opt/sherlock-git/ \
   || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
 pip3 install --progress-bar off -r /opt/sherlock-git/requirements.txt \
@@ -288,8 +322,8 @@ popd >/dev/null
 
 ##### Install Gasmask
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Gasmask${RESET}"
-apt -y -qq install git \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install git \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 git clone -q -b master https://github.com/twelvesec/gasmask.git /opt/gasmask-git/ \
   || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
 pip3 install --progress-bar off -r /opt/gasmask-git/requirements.txt \
@@ -300,8 +334,8 @@ popd >/dev/null
 
 ##### Install Sublist3r
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}Sublist3r${RESET}"
-apt -y -qq install git \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt-get -y -qq install git \
+  || echo -e ' '${RED}'[!] Issue with apt-get install'${RESET} 1>&2
 git clone -q -b master https://github.com/aboul3la/Sublist3r.git /opt/sublist3r-git/ \
   || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
 pip3 install --progress-bar off -r /opt/sublist3r-git/requirements.txt \
@@ -312,15 +346,10 @@ popd >/dev/null
 
 ##### Install PhoneInfoga
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}PhoneInfoga${RESET}"
-apt -y -qq install git \
-  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
-git clone -q -b master https://github.com/sundowndev/PhoneInfoga.git /opt/phoneInfoga-git/ \
-  || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pip3 install --progress-bar off -r /opt/phoneInfoga-git/requirements.txt \
-  || echo -e ' '${RED}'[!] Issue when pip3 installing requirements'${RESET} 1>&2
-pushd /opt/phoneInfoga-git/ >/dev/null
-git pull -q
-popd >/dev/null
+curl -s -L "https://github.com/sundowndev/phoneinfoga/releases/download/v2.0.5/phoneinfoga_$(uname -s)_$(uname -m).tar.gz" -o /opt/phoneinfoga.tar.gz \
+  || echo -e ' '${RED}'[!] Issue with downloading'${RESET} 1>&2
+mkdir /opt/phoneInfoga-git && tar xfv /opt/phoneinfoga.tar.gz -C /opt/phoneInfoga-git/ \
+cp /opt/phoneInfoga-git/PhoneInfoga /usr/bin/phoneinfoga
 
 ##### pip installs
 
@@ -346,9 +375,10 @@ pip3 install --progress-bar off instaLooter \
 ##### Clean the system
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) ${GREEN}Cleaning${RESET} the system"
 #--- Clean package manager
-for FILE in clean autoremove; do apt -y -qq "${FILE}"; done
-apt -y -qq purge $(dpkg -l | tail -n +6 | egrep -v '^(h|i)i' | awk '{print $2}')   # Purged packages
+for FILE in clean autoremove; do apt-get -y -qq "${FILE}"; done
+apt-get -y -qq purge $(dpkg -l | tail -n +6 | egrep -v '^(h|i)i' | awk '{print $2}')   # Purged packages
 #--- Update slocate database
+apt-get -y -qq install locate
 updatedb
 #--- Reset folder location
 cd ~/ &>/dev/null
@@ -360,9 +390,22 @@ done
 
 ##### Done!
 echo -e "\n ${YELLOW}[i]${RESET} Don't forget to:"
-echo -e " ${YELLOW}[i]${RESET} + Check the above output (Did everything install? Any errors? (${RED}HINT: What's in RED${RESET}?)"
+echo -e " ${YELLOW}[i]${RESET} + Check the log file (Did everything install? Any errors? (${RED}HINT: What's in RED${RESET}? the word 'issue' is in the log?)"
 echo -e " ${YELLOW}[i]${RESET} + ${YELLOW}Reboot${RESET}"
 (dmidecode | grep -iq virtual) \
   && echo -e " ${YELLOW}[i]${RESET} + Take a snapshot   (Virtual machine detected)"
+
+##### Time taken
+finish_time=$(date +%s)
+echo -e "\n\n ${YELLOW}[i]${RESET} Time (roughly) taken: ${YELLOW}$(( $(( finish_time - start_time )) / 60 )) minutes${RESET}"
+echo -e " ${YELLOW}[i]${RESET} Stages skipped: $(( TOTAL-STAGE ))"
 echo -e '\n'${BLUE}'[*]'${RESET}' '${BOLD}'Done!'${RESET}'\n\a'
 exit 0
+
+
+# TODO:
+# add spinner to visualize script didn't Heading
+# Add TL packages from sheet
+# consider replacing apt-get with apt-get
+# identify if SSH and suggest running from a screen
+# clean up output (only print stages & errors)?
